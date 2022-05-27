05be64a13e733ffddbc7f6b9c8d7dd95033e4e34
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2022-05-27 01:01:11 -0500

    Add some tests for QLocal2

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,27 @@ using StaticArrays
 using Test
 
 
-@testset "QLocal" begin
+@testset "QLocal2" begin
+    q = QLocal(1.0, 2)
+
+    @testset "Indexing" begin
+        @test size(q) == (2, 2)
+        @test q[1, 1] == -q[2, 2] == 1.0
+        @test q[2, 1] == q[2, 1] == q[2] == 2.0
+    end
+
+    @testset "Linear algebra" begin
+        @test q^2 == q * q == SMatrix(q)^2
+        @test typeof(q^2) === typeof(q * q) === QLocal2{Float64}
+
+        trq², trq³ = LiquidCrystals.tr_sq_cb(q)
+        @test LiquidCrystals.tr_sq(q) == tr(q^2) == trq²
+        @test LiquidCrystals.tr_cb(q) == tr(q^3) == trq³ == 0.0
+    end
+end
+
+
+@testset "QLocal3" begin
     q = QLocal(1, 2, 3, 4, 5, -5.0)
 
     @testset "Indexing" begin
@@ -15,7 +35,7 @@ using Test
 
     @testset "Linear algebra" begin
         @test q^2 == q * q == SMatrix(q)^2
-        @test typeof(q^2) === typeof(q * q) === QLocal{3, Float64, 6}
+        @test typeof(q^2) === typeof(q * q) === QLocal3{Float64}
 
         trq², trq³ = LiquidCrystals.tr_sq_cb(q)
         @test LiquidCrystals.tr_sq(q) == tr(q^2) == trq²
