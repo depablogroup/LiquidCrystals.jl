ebd21bb6f8639f49c1d2a89c2e531f81fb47186d
j-salmeron <95516065+j-salmeron@users.noreply.github.com>
2022-06-07 18:36:08 -0500

    Update operators.jl
    Axis BX extension

--- src/finitediff/operators.jl
+++ src/finitediff/operators.jl
@@ -152,18 +152,14 @@ function Base.:*(Op::CenteredDifference{2}, A::AbstractMatrix)
     return B
 end
 
-function LinearAlgebra.mul!(B::AbstractArray, Op::CenteredDifference{N, T, Tag}, A::AbstractArray) where {N, T, Tag}
-	op = concretize(Tag)
-	sz = size(A) .- 1
-	CI = CartesianIndices(UnitRange.(2, sz))
-	#C = CartesianIndices(A)
-	coeffs = Op.coeffs
-	# While not on the edges
-	#for I in C[2:(end - 1), 2:(end - 1)]
-	for I in CI
-		B[I] = op(A, I, coeffs)
-	end
-	return B
+function LinearAlgebra.mul!(B, Op::CenteredDifference{2}, A::AbstractMatrix)
+    C = CartesianIndices(A)
+    coeffs = Op.coeffs
+    # While not on the edges
+    for I in C[2:(end - 1), 2:(end - 1)]
+        B[I] = laplacian(A, I, coeffs)
+    end
+    return B
 end
 
 function gradient(A, I, coeffs::NTuple{2})
@@ -227,6 +223,20 @@ function laplacian(A, I, coeffs::NTuple{2})
 end
 
 function laplacian(A, I, coeffs::NTuple{3})
+	Ix = CartesianIndex(0,0,1)
+	Iy = CartesianIndex(0,1,0)
+	Iz = CartesianIndex(1,0,0)
+
+	Cx, Cy, Cz = coeffs
+	
+	return (
+		Cx[1] * A[I - Ix] + Cx[2] * A[I] + Cx[3] * A[I + Ix] +
+		Cy[1] * A[I - Iy] + Cy[2] * A[I] + Cy[3] * A[I + Iy] +
+		Cz[1] * A[I - Iz] + Cz[2] * A[I] + Cz[3] * A[I + Iz]
+	)
+end
+
+function laplacian(A, I, coeffs::NTuple{3})
 	Iz = CartesianIndex(0,0,1)
 	Iy = CartesianIndex(0,1,0)
 	Ix = CartesianIndex(1,0,0)
@@ -280,249 +290,6 @@ function build_caches(op, bc::DirichletBC{2}, u0) #do we need op?
     extended[Cl_h2] .= bc.c_h[2]
 
     return extended, result
-end
-
-function apply_BCs(op, u, box::BoxBC, extended)
-	sz = size(extended) .- 1
-	CI = CartesianIndices(UnitRange.(2, sz))
-	extended[CI] .= u
-
-	for bc in box.bcs
-		apply_BCs(op, u, bc, extended)
-	end
-
-	return extended
-end
-
-apply_BCs(op, u, bc::DirichletAxisBC, extended) = extended
-
-function build_caches(op, box::BoxBC, u0) #do we need op?
-	sz = size(u0) .+ 2
-	
-	extended = zeros(sz)
-	result = copy(extended)
-
-	for bc in box.bcs
-		initialize(op, bc, extended)
-	end
-
-	
-	return extended, result
-
-end
-
-function initialize(op, bc::AxisBC, extended)
-
-	return extended
-
-end
-
-function get_dr(::Type{XAxis})
-	return 1
-end
-
-function get_dr(::Type{YAxis})
-	return 2
-end
-
-function get_dr(::Type{ZAxis})
-	return 3
-end
-
-function initialize(op, bc::DirichletAxisBC{A}, extended) where {A <: Axis}
-	sz = size(extended)
-	lo, hi = ghost_ranges(sz, A) 
-	
-	CI_lo = CartesianIndices(lo)       #low-value of x-axis
-	extended[CI_lo] .= bc.c_l
-
-	CI_hi = CartesianIndices(hi)      #high-value of x-axis
-	extended[CI_hi] .= bc.c_h
-
-	return extended
-
-end
-
-function apply_BCs(op, u, bc::NeumannAxisBC{A}, extended)  where {A <: Axis}
-
-	axis=get_dr(A)
-	sz = size(extended)
-	lo_g, hi_g = ghost_ranges(sz, A) 
-
-	CI_lb, CI_hb = boundary_ranges(sz, bc)
-
-
-	
-	CI_lg = CartesianIndices(lo_g)  #low-value of axis
-	extended[CI_lg] .= u[CI_lb] .+ 2*bc.c_l*op.dr[axis]  
-
-	CI_hg = CartesianIndices(hi_g)  #high-value of axis
-	extended[CI_hg] .= u[CI_hb]  .- 2*bc.c_h*op.dr[axis]  
-
-
-	return extended
-
-end
-
-function apply_BCs(op, u, bc::PeriodicAxisBC{A}, extended) where {A <: Axis}
-
-	sz = size(extended)
-	lo_g, hi_g = ghost_ranges(sz, A) 
-
-	CI_lb, CI_hb = boundary_ranges(sz, bc)
-	
-	CI_lg = CartesianIndices(lo_g)  #low-value of x-axis
-	extended[CI_lg] .= u[CI_lb]   
-
-	CI_hg = CartesianIndices(hi_g)  #high-value of x-axis
-	extended[CI_hg] .= u[CI_hb]  
-
-
-	return extended
-
-end
-
-function apply_BCs(op, u, bc::RobinAxisBC{A}, extended) where {A <: Axis}
-
-	axis=get_dr(A)
-	sz = size(extended)
-	lo_g, hi_g = ghost_ranges(sz, A) 
-
-	CI_lb, CI_hb = boundary_ranges(sz, bc)
-
-
-	
-	CI_lg = CartesianIndices(lo_g)  #low-value of axis
-	extended[CI_lg] .=  (bc.c_l .+ (bc.b_l / (2*op.dr[axis]))  * u[CI_lb]) / (bc.a_l + (bc.b_l / (2* op.dr[axis] ) ) ) 
-
-	CI_hg = CartesianIndices(hi_g)  #high-value of axis
-	extended[CI_hg] .= (bc.c_h .- (bc.b_h / (2*op.dr[axis])) * u[CI_hb]) / (bc.a_h - (bc.b_h / (2* op.dr[axis] ) )) 
-
-
-	return extended
-
-end
-
-function ghost_ranges(sz::NTuple{2},::Type{YAxis})
-	m, n = sz
-	return (2:m-1, 1:1), (2:m-1, n:n)
-end
-
-function ghost_ranges(sz::NTuple{2},::Type{XAxis})
-	m, n = sz
-	return (1:1, 2:n-1), (m:m, 2:n-1)
-end
-
-function ghost_ranges(sz::NTuple{3},::Type{ZAxis})
-	l, m, n = sz
-	return (2:l-1, 2:m-1, 1:1), (2:l-1, 2:m-1, n:n)
-end
-
-function ghost_ranges(sz::NTuple{3},::Type{YAxis})
-	l, m, n = sz
-	return (2:l-1, 1:1, 2:n-1), (2:l-1, m:m, 2:n-1)
-end
-
-function ghost_ranges(sz::NTuple{3},::Type{XAxis})
-	l, m, n = sz
-	return (1:1, 2:m-1, 2:n-1), (l:l, 2:m-1, 2:n-1)
-end
-
-const DerivativeAxisBC{T} = Union{NeumannAxisBC{T}, RobinAxisBC{T}}
-
-function boundary_ranges(sz::NTuple{2}, ::DerivativeAxisBC{YAxis})
-	m, n = sz
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((1:m-2, 2:2))    
-	hi = CartesianIndices((1:m-2, (n-2-1):(n-2-1)))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{2}, ::DerivativeAxisBC{XAxis})
-	m, n = sz
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((2:2, 1:n-2))    
-	hi = CartesianIndices((m-2-1:m-2-1, 1:n-2))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{3}, ::DerivativeAxisBC{ZAxis})
-	l, m, n = sz
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((1:l-2,1:m-2, 2:2))    
-	hi = CartesianIndices((1:l-2,1:m-2, (n-2-1):(n-2-1)))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{3}, ::DerivativeAxisBC{YAxis})
-	l, m, n = sz
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((1:l-2,2:2, 1:n-2))    
-	hi = CartesianIndices((1:l-2,m-2-1:m-2-1,1:n-2))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{3}, ::DerivativeAxisBC{XAxis})
-	l, m, n = sz
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((2:2,1:m-2, 1:n-2))    
-	hi = CartesianIndices((l-2-1:l-2-1,1:m-2, 1:n-2))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{2}, ::PeriodicAxisBC{YAxis})
-	m, n = sz
-
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((1:m-2, n-2:n-2))    
-	hi = CartesianIndices((1:m-2, 1:1))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{2}, ::PeriodicAxisBC{XAxis})
-	m, n = sz
-
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((m-2:m-2, 1:n-2))    
-	hi = CartesianIndices((1:1, 1:n-2))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{3}, ::PeriodicAxisBC{ZAxis})
-	l, m, n = sz
-
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((1:l-2,1:m-2, n-2:n-2))    
-	hi = CartesianIndices((1:l-2,1:m-2, 1:1))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{3}, ::PeriodicAxisBC{YAxis})
-	l, m, n = sz
-
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((1:l-2,m-2:m-2, 1:n-2))    
-	hi = CartesianIndices((1:l-2,1:1, 1:n-2))
-
-	return lo, hi
-end
-
-function boundary_ranges(sz::NTuple{3}, ::PeriodicAxisBC{XAxis})
-	l, m, n = sz
-
-	#extract two as it is the extended matrix size
-	lo = CartesianIndices((l-2:l-2,1:m-2, 1:n-2))    
-	hi = CartesianIndices((1:1,    1:m-2, 1:n-2))
-
-	return lo, hi
 end
 
 function apply_BCs(op, u, bc::DirichletBC{2}, extended)
